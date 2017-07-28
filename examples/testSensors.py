3e24e56e0612dab58aaadeaeb3a6bee8447a2402
Timothy Yong <tyong_23@hotmail.com>
2017-07-28 18:27:08 -0400

    add syspath

--- examples/testSensors.py
+++ examples/testSensors.py
@@ -1,19 +1,20 @@
+import sys
+sys.path.append("../perception")
 import perception
 
-def displayDistribution(grid):
+def displayDistribution(name, grid):
   import matplotlib.pyplot as plt
   from matplotlib import cm
 
-  plt.imshow(grid, cmap=cm.gray)
+  plt.imshow(name, np.flipud(grid) * 255.0, cmap=cm.gray)
   plt.show()
 
 
 values = [50,50,50,50,50,50,50,50,50]
 
 grid = perception.getSonarDistribution(values)
+displayDistribution("Sonar", grid)
 #grid = perception.getLidarDistribution(values)
+#displayDistribution("Lidar", grid)
 #grid = perception.getZEDDistribution(values)
-displayDistribution(grid)
-
-
-
+#displayDistribution("ZED", grid)
