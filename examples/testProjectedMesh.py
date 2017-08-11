c286887fb83d4e782610ba285e8fac3a897fbfc3
Timothy Yong <tyong_23@hotmail.com>
2017-08-11 02:19:55 -0400

    add zed to projection

--- examples/testProjectedMesh.py
+++ examples/testProjectedMesh.py
@@ -1,19 +1,24 @@
 import sys
+sys.path.append("../device/")
 sys.path.append("../perception/")
-from projectedMesh import project3DMeshTo2DGrid
+import zed
+import projectedMesh
 import numpy as np
+import cv2
 
-triangles = np.array([
-  [0, 1, 2],
-  [3, 1, 2],
-  [3, 0, 2],
-  [1, 0, 4]])
+while True:
+  pose = zed.getPose()
+  x = pose[0]
+  y = pose[1]
+  theta = pose[6]
+  pos = np.array((x, y))
 
-vertices = [
-    (100, 100, 100),
-    (100, 200, 300),
-    (200, 100, 50),
-    (200, 200, 200),
-    (50, 50, 0)]
+  vt = zed.getMeshData()
+  v = vt[:,0]
+  v.reshape((v.shape[0] / 3, 3))
+  t = vt[:,1]
+  t.reshape((t.shape[0] / 3, 3))
 
-project3DMeshTo2DGrid(vertices, triangles, (100.0, 100.0))
+  mesh = projectedMesh.project3DMeshTo2DGrid(v, t, (x, y))
+  cv2.imshow("mesh", np.flipud(mesh) * 255)
+  cv2.waitKey(10)
