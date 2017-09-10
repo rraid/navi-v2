05caf0d266eb28123f212cadfc23e6e4a4f0bef4
Timothy Yong <tyong_23@hotmail.com>
2017-09-10 05:58:29 -0400

    added naive test of perceptbox just to see if it compiles correctly

--- examples/testPerceptbox.py
+++ examples/testPerceptbox.py
@@ -1,6 +1,70 @@
 import sys
 sys.path.append("../perception/")
 import perceptbox
+import numpy as np
 
+timestep = 0
 
-PerceptBox.init()
+def getGridImg():
+  return np.ones((400, 400))
+
+def getGPS():
+  readings = [
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0) ]
+  return readings[timestep]
+
+def getCompass():
+  readings = [
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0) ]
+  return readings[timestep]
+
+def getZed():
+  readings = [
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0),
+      (0, 0) ]
+  return readings[timestep]
+
+if __name__ == "__main__":
+  # create a perception box to be used on its own
+  box = perceptbox.PerceptBox(getGridImg(),
+      gpsCallback=getGPS, compassCallback=getCompass, stereoPoseCallback=getZed)
+
+  # now that all the callbacks are set, let's create an anchor for the robot to
+  # anchor the position
+  box.anchor()
+  box.start()
+
+  # now that the box has been anchored, we can attempt to test out the localizer
+  while True:
+
+    print(box.getPose())
+
+    if timestep < 9:
+      timestep += 1
+
--- perception/perceptbox.py
+++ perception/perceptbox.py
@@ -2,28 +2,36 @@ import sys
 sys.path.append("../device/")
 import perception
 import devhub
-import pfilter
 from threading import Thread
+import time
+import numpy as np
 
 class PerceptBox(Thread):
   """
-  This class initializes all the functions of the sensors and distributions that
-  we have into one single object instance. This includes threads that have to be
-  run, as well as memory allocation, syncronization, and sharing. By managing
-  the sensors in this fashion, we are able to speed up the perception process,
-  and output a more simple model of our environment.
+  Acts as the front end for the perception unit. This unit contains two buffers:
+  1) Global Buffer: a buffer which contains (GPS, Compass) readings
+  2) Local Buffer: a buffer which contains more accurate stereo readings
 
-  Note that this is the first version of the box which uses a online-learned
-  calibration technique for correlation between the box and the sensor readings.
+  Note that this is the first version of the box which uses a mean-shifted
+  correlation technique to connect the local and global frames.
   """
-  def __init__(self, gridimg, initial_pose=None, sensorport=None):
-    Thread.__init__(self)
+  def __init__(self, gridimg, initial_pose=None,
+      gpsCallback=None,
+      compassCallback=None,
+      stereoPoseCallback=None):
     """ Constructor
     Arguments:
     - gridimg: numpy array of dimensionality (rows, cols)
     - initial_pose: (x, y, theta) initial pose of the robot
-    - sensorport: the port of the arduino that has the sensors
+    - gpsCallback: a function which returns the gps reading (x, y)
+    - compassCallback: a function which returns the compass reading theta
+    - stereoPoseCallback: a function which returns (x, y, theta)
     """
+    assert(type(gpsCallback) != type(None))
+    assert(type(compassCallback) != type(None))
+    assert(type(stereoPoseCallback) != type(None))
+
+    Thread.__init__(self)
     # shape = (y, x)
     self.gridimg = gridimg
     if type(initial_pose) == type(None):
@@ -33,61 +41,94 @@ class PerceptBox(Thread):
     else:
       self.initial_pose = initial_pose
 
-    if sensorport:
-      devhub.init(megaport=sensorport, control_en=False)
-    else: # do no initialization
-      pass
-
-    # mapper
-    #self.gridmap = gridmap.GridMap()
-    #self.gridmap.positionCallback = self.getPose
-    #self.gridmap.collisionCallback = devhub.getZedReadings
-
     # start by defining a buffer to store the GPS and the robot
+    self.autoCalibrate = True
     self.bufmax = 2000 # max 2000 readings
-    self.GPSBuffer = np.zeros((bufmax, 2))
-    self.CompassBuffer = np.zeros((bufmax, 1))
-    self.ZEDBuffer = np.zeros((bufmax, 3))
-
-    # store the GPS readings until they fill up (note: this will take some time...)
-    for i in range(self.bufmax):
-      self.lastPos = devhub.getGPSReadings()
-      self.lastTheta = devhub.getCompassReadings()
-      self.lastPose = zed.getPose()
-      self.GPSBuffer[i] = self.lastPos
-      self.CompassBUffer[i] = self.lastTheta
-      self.ZEDBuffer[i] = self.lastPose
+    self.globalBuffer = np.zeros((0, 3))
+    self.localBuffer = np.zeros((0, 3))
 
     # learning an initial transformation
     self.transform = np.array([0, 0, 0], dtype=np.float32)
-    self.update()
+
+    # create a counter for current frame index
+    self.frameid = 0
+    self.gpsCallback = gpsCallback
+    self.compassCallback = compassCallback
+    self.stereoPoseCallback = stereoPoseCallback
+    self.globalPose = None
+    self.localPose = None
+    self.calibratedPose = np.array([0, 0, 0], dtype=np.float32)
 
   def update(self):
+    """ Update the current pose based on the buffers of stored data
+    """
+    # update the values inside the buffers
+    self.globalPose = np.concatenate(
+        [self.gpsCallback(), self.compassCallback()], axis=1)
+    self.localPose = self.stereoPoseCallback()
+    if self.globalPose.shape[0] < self.bufmax:
+      self.globalBuffer = np.concatenate(self.globalBuffer,
+          np.reshape(self.globalPose, (1, 3)))
+      self.localBuffer = np.concatenate(self.localBuffer,
+          np.reshape(self.localPose, (1, 3)))
+    else:
+      self.globalBuffer[self.frameid, :] = self.globalPose
+      self.localBuffer[self.frameid, :] = self.localPose
+
     # assume that the compass is always right (later on use sin and cos rules)
-    theta = np.mean(self.CompassBuffer - self.ZEDBuffer[:,2:])
-    # use the transformation to obtain the offset
-    s = math.sin(math.radians(theta))
-    c = math.cos(math.radians(theta))
-    newpos = np.dot(self.ZEDBuffer[:,:2], np.array([[c, s], [-s, c]]))
-    # grab the offset from the matrix transform
-    offset = np.mean(self.GPSBuffer - newpos)
-    self.transform[:2] = offset
-    self.transform[2] = theta
+    if self.autoCalibrate:
+      thetaOffset = np.mean(self.globalBuffer[:,2:] - self.localBuffer[:,2:])
+      # use the transformation to obtain the offset
+      s = math.sin(math.radians(thetaOffset))
+      c = math.cos(math.radians(thetaOffset))
+      newpos = np.dot(self.localBuffer[:,:2], np.array([[c, -s], [s, c]]))
+      # grab the offset from the matrix transform
+      posOffset = np.mean(self.globalBuffer[:,:2] - newpos)
+      self.transform[:2] = posOffset
+      self.transform[2] = thetaOffset
+    else:
+      # set the offsets to the old values
+      thetaOffset = self.transform[2]
+      posOffset = self.transform[:2]
+      s = math.sin(math.radians(thetaOffset))
+      c = math.cos(math.radians(thetaOffset))
+      newpos = np.dot(self.localPose[:2], np.array([[c, -s], [s, c]]))
+
+    # use the transform to estimate the calibrated pose
+    self.calibratedPose = np.concatenate(
+        [newpos[self.frameid, :], [self.localBuffer[2] + thetaOffset]])
+    self.frameid = (self.frameid + 1) % self.bufmax
+
+  def run(self):
+    while True:
+      self.update()
 
   def getPose(self):
     """
     Returns:
     - pose (x, y, theta)
     """
-    t = self.transform[2]
-    s = math.sin(math.radians(t))
-    c = math.cos(math.radians(t))
-    R = np.array([[c, s], [-s, c]])
-    xy = np.dot(self.lastPose[:2], R) + self.transform[:2]
-    return np.array([xy[0], xy[1], t + self.transform[2]])
+    return np.copy(self.calibratedPose)
 
   def getMap(self, radius=10.0, scaling=0.1):
     """
     Get the distribution of the map, given a radius
     """
     # for now just test the localizer to see if it works
+    pass
+
+  def anchor(self):
+    """ Turn off automatic calibration and use the current set of readings for
+        the offset.
+    """
+    self.autoCalibrate = False
+
+  ## Callbacks ##
+  def assignGPSCallback(self, fn):
+    self.gpsCallback = fn
+
+  def assignCompassCallback(self, fn):
+    self.compassCallback = fn
+
+  def assignStereoPoseCallback(self, fn):
+    self.stereoPoseCallback = fn
