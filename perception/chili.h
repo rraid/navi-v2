eab3cea339841dcccc4b7b30edd19b7eb2cefefc
unshure <nac542@gmail.com>
2017-11-08 22:11:21 -0500

    removed useless garbage

--- perception/chili.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CHILI_H__
-#define __CHILI_H__
-
-extern "C" {
-  int getNumDetect(void *img, int width, int height);
-  void copyAndClean(void *__ids__, void *__buf__);
-}
-
-#endif
--- perception/pathLoader.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-def load(fname):
-  world = np.flipud(cv2.imread(fname, cv2.IMREAD_GRAYSCALE) > 128)\
-      .astype(np.float32)
-  return world
